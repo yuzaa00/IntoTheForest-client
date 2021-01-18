476453411f6eb3522a028d172f17a6087b1b44da
이소정 <sjlee950326@gmail.com>
2021-01-18 17:22:47 +0900

    :hammer: setting directory

---
+++ src/redux/actions/counter.ts
@@ -0,0 +1,16 @@
+// redux/actions/counter.ts
+import { INCREASE, DECREASE } from './types';
+
+// 액션 생성함수를 선언합니다
+export const increase = () => ({
+  type: INCREASE
+});
+
+export const decrease = () => ({
+  type: DECREASE
+});
+
+// export const increaseBy = (diff: number) => ({
+//   type: INCREASE_BY,
+//   payload: diff
+// });
