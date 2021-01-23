ea8bf2958cce3cf6b5a5e734c99ee96355df0424
이소정 <sjlee950326@gmail.com>
2021-01-23 20:40:01 +0900

    :white_check_mark: story description page

---
+++ src/components/ChoiceCharacter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChoiceCharacter = () => {
+    return (
+        <div>
+            ChoiceCharacter Page
+        </div>
+    )
+}
+
+export default ChoiceCharacter;
