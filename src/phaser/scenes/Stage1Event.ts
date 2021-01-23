8802e2c27f9d73427fb6ebd9d18a14c38955856e
Youn_Ji <syj9760@gmail.com>
2021-01-24 00:18:23 +0900

    :lipstick: MiniGame Alert

--- src/phaser/scenes/Stage1Event.ts
+++ src/phaser/scenes/Stage1Event.ts
@@ -1,6 +1,7 @@
 export default class Stage1Event extends Phaser.Scene {
 
   private popup!: any
+  private title!: any
   private button!: any
   private startButton!: any
 
@@ -16,60 +17,75 @@ export default class Stage1Event extends Phaser.Scene {
     this.registry.set('squi', data.squi)
   }
 
+  
+
   preload(): void {
+    this.add.image(0, 0, 'cardbg').setOrigin(0, 0).setDepth(0)
 
     this.popup = this.add.graphics()
-    this.popup.lineStyle(1, 0x2a275c)
-    this.popup.fillStyle(0x00ff00, 0.6) // 배경색
-    this.popup.strokeRect(25, 25, 750, 550)
-    this.popup.fillRect(25, 25, 750, 550)
+    this.popup.lineStyle(20, 0x501f0e)
+    this.popup.fillStyle(0xffffff) // 배경색, 투명도
+    this.popup.strokeRoundedRect(300, 120, 600, 400)
+    this.popup.fillRoundedRect(300, 120, 600, 400)
+
+    this.title = this.add.graphics()
+    this.title.fillStyle(0x501f0e) // 배경색
+    this.title.fillRoundedRect(450, 85, 300, 60, 30)
+
     //start button square
     this.button = this.add.graphics()
     this.button.lineStyle(1, 0x2a275c)
     this.button.fillStyle(0xf6d304, 0.5)
-    this.button.strokeRect(325, 465, 150, 50)
-    this.button.fillRect(325, 465, 150, 50)
+    this.button.strokeRoundedRect(525, 440, 150, 50, 5)
+    this.button.fillRoundedRect(525, 440, 150, 50, 5)
 
     this.add
-      .text(400, 83, "보너스 게임", {
-        color: '#bfff00',
+      .text(600, 117, "보너스 게임", {
+        color: '#ffffff',
         fontSize: '40px',
         fontStyle: 'bold',
-      })
-      .setOrigin(0.5)
-
+      }).setOrigin()
+      
     this.add
-      .text(
-        400,
-        200,
-        '주어진 시간 30초 동안 최대한 많은 수의 카드를 뒤집으세요! 카드를 맞추면 체력이 회복됩니다!',
-        {
-          color: '#CED4D6',
+      .text(600, 200, '주어진 시간 30초 동안', {
+          color: '#000000',
           fontSize: '20px',
           fontStyle: 'bold',
           align: 'center',
           wordWrap: { width: 480, useAdvancedWrap: true },
-        }
-      )
-      .setOrigin(0.5)
+      }).setOrigin()
 
     this.add
-      .text(400, 400, '작동방법 : 카드를 마우스로 클릭', {
-        color: '#CED4D6',
-        fontSize: '20px',
-        fontStyle: 'bold',
-        align: 'center',
-      })
-      .setOrigin(0.5)
+      .text(600, 250, '최대한 많은 수의 카드를 뒤집으세요!', {
+          color: '#000000',
+          fontSize: '20px',
+          fontStyle: 'bold',
+          align: 'center',
+      }).setOrigin()
+
+    this.add
+      .text(600, 300, '카드를 맞추면 보너스 체력을 얻습니다', {
+          color: '#000000',
+          fontSize: '20px',
+          fontStyle: 'bold',
+          align: 'center',
+      }).setOrigin()
+      
+    this.add
+      .text(600, 380, '작동방법 : 카드를 마우스로 클릭', {
+          color: '#000000',
+          fontSize: '20px',
+          fontStyle: 'bold',
+          align: 'center',
+      }).setOrigin()
 
     this.startButton = this.add
-      .text(400, 490, 'Start', {
+      .text(600, 465, 'Start', {
         color: '#2A275C',
         fontSize: '30px',
         fontStyle: 'bold',
-      })
-      .setOrigin(0.5)
-    this.startButton.setInteractive()
+      }).setOrigin()
+    
     this.startButton.on(
       'pointerdown',
       () => {
@@ -79,6 +95,4 @@ export default class Stage1Event extends Phaser.Scene {
       this
     )
   }
-
-
 }
