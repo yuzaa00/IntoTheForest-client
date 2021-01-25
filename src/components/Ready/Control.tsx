5115d6fcff5897261831c31c074a4a92dc54a298
이소정 <sjlee950326@gmail.com>
2021-01-25 19:12:11 +0900

    :lipstick: modify canvas and completed control page

--- src/components/Ready/Control.tsx
+++ src/components/Ready/Control.tsx
@@ -1,11 +1,66 @@
-import React, { useState, useEffect }from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState  }from 'react';
+import { Link } from 'react-router-dom';
+import './Control.css';
 
 function Control() {
 
-  return (
-    <div>조작법 설명</div>
-  );
+    const controlDescription = [
+        { name: '점수 획득 아이템', data: ['뼈다귀'], img: '../../images/object/bone.png' },
+        { name: '몬스터', data: ['뱀', '멧돼지', '늑대'], img: '../../images/character/all-monster.png' },
+        { name: '장애물(체력 감소)', data: ['독버섯'], img: '../../images/object/mushroom.png' },
+        { name: '서브캐', data: ['서브캐 증가'], img: '../../images/character/subcharacters.png' },
+        { name: '포션', data: ['블루 포션 (체력 소폭 증가)', '레드 포션 (체력 대폭 증가)', '스테이지 포션 (스테이지 전환)'], img: '../../images/object/all-potion.png' },
+    ];
+
+    const [controlModal, setControlModal] = useState<boolean>(false);
+
+    return (
+      <div className="control-canvas">
+        <h2>
+          {!controlModal? '게임 설명':'조작키 설명'}
+        </h2>
+        <div className="description-area">
+            {!controlModal && (
+              <div>
+                <div className="control-description">
+                  {controlDescription.map(item => {
+                        return (
+                            <div className="control-layout">
+                              <img src={item.img} />
+                                <h3>{item.name}</h3>
+                                <ul className="item-data">
+                                  {item.data.map(el => {
+                                    return <li>{el}</li>
+                                  })}
+                                </ul>
+                            </div>
+                        )
+                    })}
+                </div>
+                <p className="first-p">주 캐릭터가 몬스터 또는 장애물을 피하지 못했을 경우,</p>
+                <p className="second-p">주 캐릭터의 체력이 깎이거나 서브캐릭터를 보유하고 있다면 서브캐릭터 감소</p>
+            </div>
+            )}
+            {controlModal && (
+              <div className="game-control">
+                  <p>기본: 자동 달리기</p>
+                  <p>점프: 화면 좌측 하단 점프 버튼 또는 스페이스바 클릭</p>
+              </div>
+            )}
+        </div>
+
+        <div className="button-area">
+            <button className="description control-button" onClick={() => setControlModal(!controlModal)}>
+              {!controlModal? '조작키 설명' : '게임 설명'}
+            </button>
+            <Link to="/game">
+              <button className="start control-button">
+                GAME START
+              </button>
+            </Link>
+          </div>
+      </div>
+    )
 }
 
 export default Control;
--- src/components/Ready/Story.tsx
+++ src/components/Ready/Story.tsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Story.css';
-import ChoiceCharacter from './ChoiceCharacter';
 
-const Story = () => {
+function Story () {
 
     const [showMoreButton, setShowMoreButton] = useState<boolean>(false);
     const [showSkipButton, setShowSkipButton] = useState<boolean>(false);
     const [showText, setShowText] = useState<boolean>(false);
-    const [skipPage, setSkipPage] = useState<boolean>(false);
 
     const text1:string = '어쩌고 저쩌고... 뭐는 이랬고 뭐는 저랬고 우당탕탕 스펙타클한 갱얼쥐의 12시간동안 뛰댕기는 이야기...';
     const text2:string = '그리고 뭐 이렇게 됐다가 저렇게 됐다가 왔다갔다 하고... 갱얼쥐는 숲 속에서 몬스터도 만나고 칭구들도 만날 수 있으니 도전해보세요!';
@@ -29,13 +27,7 @@ const Story = () => {
         setShowText(true);
     }
 
-    // 페이지 전환 (캐릭터 선택창)
-    // const skipStory = ():void => {
-    //     setSkipPage(true);
-    // }
-
     return (
-     
                 <div className="full-screen">
                     <h2>게임 스토리 설명</h2>
                     <div className="story-area">
@@ -56,13 +48,10 @@ const Story = () => {
                         </div>
                     </div>
                     <div className="moreOrSkip">
-                        {showMoreButton && <button className="more" onClick={moreStory}>더보기</button>}
-                        {showSkipButton && <Link to="/ready/character"><button className="skip">SKIP</button></Link>}
+                        {showMoreButton && <button className="more story-button" onClick={moreStory}>더보기</button>}
+                        {showSkipButton && <Link to="/character"><button className="skip story-button">SKIP</button></Link>}
                     </div>
                 </div>
-      
-      
-       
     )
 }
 
