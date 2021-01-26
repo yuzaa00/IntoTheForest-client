f867a49d14f0c552a30c1afb9fadb72d3ef6885e
JaejinLee <ppaimar16@gmail.com>
2021-01-26 15:42:00 +0900

    :art: [React] 캐릭터 선택페이지 구현 3
    캐릭터 선택창 CSS, 버튼

--- src/components/Ready/ChoiceCharacter.tsx
+++ src/components/Ready/ChoiceCharacter.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import John from '../../images/character/john.png'
+import Tom from '../../images/character/backgu.png'
+import Alex from '../../images/character/dosaDog.png'
 
 type ChoiceCharacterProps {
     value: string;
@@ -10,24 +13,53 @@ type ChoiceCharacterProps {
 function ChoiceCharacter({ value, onSelect1, onSelect2, onSelect3 }:ChoiceCharacterProps) {
 
     
-    // const [value, setValue] = useState("")
-    // const onSelect1 = () => {
-    //    setValue("멍멍이")
-    // }
-    // const onSelect2 = () => {
-        // setValue("치와와")
-    //  }
+   
+    const onGameDesc = () => {
+      //history
+     }
+    const onGameStart = () => {
+      {value}
+      //history.push
+    }
     //  const onSelect3 = () => {
         // setValue("포메라이안")
     //  } 
 
     return (
-      <div>
+      <div className="chaSelectBx">
         <h1>{value}</h1>
         <div>
-          <button onClick={onSelect1}>"player1"</button>
-          <button onClick={onSelect2}>"player2"</button>
-          <button onClick={onSelect3}>"player3"</button>
+          <div onClick={onSelect1} className="box">
+            <div className="imgBx">
+              <img src={John} alt='john' />
+            </div>
+            <div className="imgBxContent">
+             <h2>John{"\n"}<span>믹스견
+               </span></h2>
+            </div>
+          </div>
+          <div onClick={onSelect2} className="box">
+            <div className="imgBx">
+            <img src={Tom} alt='tom' />
+            </div>
+            <div className="imgBxContent">
+             <h2>Tom{"\n"}<span>진돗개
+               </span></h2>
+            </div>
+          </div>
+          <div onClick={onSelect3} className="box">
+            <div className="imgBx">
+            <img src={Alex} alt='alex' />
+            </div>
+            <div className="imgBxContent">
+             <h2>Alex{"\n"}<span>도사견
+               </span></h2>
+            </div>
+          </div>
+        </div>
+        <div>
+          <button onClick={onGameDesc}>"게임설명"</button>
+          <button onClick={onGameStart}>"GameStart"</button>
         </div>
       </div>
     );
