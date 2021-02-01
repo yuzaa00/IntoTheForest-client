917b7d20f1a88d8ae78c9102611d1e4a96e995bf
Youn_Ji <syj9760@gmail.com>
2021-02-01 18:19:44 +0900

    css3

--- src/components/Room/RoomInfo.tsx
+++ src/components/Room/RoomInfo.tsx
@@ -30,7 +30,7 @@ function RoomInfo() {
   // }, [isReady])
 
   return (
-    <div className="Infocontainer">🔥 READY 🔥
+    <div className="Infocontainer"> 레디 상태
       {store.getState().roomReducer.users.map((user, idx) => (
         <div key={idx}>
           <div className="Infocontent">{user.nickName} {user.ready ? '✅' : '☑️'} </div>
