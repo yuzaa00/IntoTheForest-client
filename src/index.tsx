fe114ecf4c1d5b5d56396608e9147c89e3419331
urusara13 <urusara13@gmail.com>
2021-02-06 10:34:05 +0900

    clean

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
-import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './redux/rootReducer'
-import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import logger from 'redux-logger'
 
 export const store = createStore(rootReducer)
 
@@ -19,9 +18,4 @@ ReactDOM.render(
     </Provider>
   </BrowserRouter>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
